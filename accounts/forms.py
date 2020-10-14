31818a6092633c0b7aaa40e0f1892471dce0abad
Stefano Passaro <stefano.passaro@gmail.com>
2020-10-14 07:06:54 +0200

    Login and reset password are fine

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=20)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password'}))
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
